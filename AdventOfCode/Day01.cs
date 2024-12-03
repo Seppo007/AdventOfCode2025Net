e27e73a6dec3ac4b0a869f069a67f19a6991217e
Sebastian Schalow <schalowseb@gmail.com>
2024-12-03 21:53:58 +0100

    Solve Day1 Part2. Refactor

--- AdventOfCode/Day01.cs
+++ AdventOfCode/Day01.cs
@@ -11,28 +11,49 @@ public sealed class Day01 : BaseDay
         _input = File.ReadAllText(InputFilePath);
     }
     
-    private int Part1()
+    private string Part1()
+    {
+        var (leftList, rightList) = GetLeftAndRightList();
+        
+        return leftList
+            .Zip(rightList, (left, right) => Math.Abs(left - right))
+            .ToArray()
+            .Sum()
+            .ToString();
+    }
+
+    private string Part2()
+    {
+        var (leftList, rightList) = GetLeftAndRightList();
+
+        var sum = 0;
+        foreach (var leftEntry in leftList)
+        {
+            sum += leftEntry * rightList.Count(right => right == leftEntry);
+        }
+        
+        return sum.ToString();
+    }
+
+    private Tuple<int[], int[]> GetLeftAndRightList()
     {
         var inputArrAsInts = Regex.Matches(_input, @"\d+")
             .Select(entry => int.Parse(entry.Value))
             .ToArray();
 
-        var leftSide = inputArrAsInts
+        var leftList = inputArrAsInts
             .Where((_, index) => index % 2 == 0)
-            .ToArray()
-            .Order();
-        var rightSide = inputArrAsInts
+            .Order()
+            .ToArray();
+        var rightList = inputArrAsInts
             .Where((_, index) => index % 2 != 0)
-            .ToArray()
-            .Order();
-
-        return leftSide
-            .Zip(rightSide, (left, right) => Math.Abs(left - right))
-            .ToArray()
-            .Sum();
+            .Order()
+            .ToArray();
+        
+        return Tuple.Create(leftList, rightList);
     }
+    
+    public override ValueTask<string> Solve_1() => new(Part1());
 
-    public override ValueTask<string> Solve_1() => new(Part1().ToString());
-
-    public override ValueTask<string> Solve_2() => new($"Solution to {ClassPrefix} {CalculateIndex()}, part 2");
+    public override ValueTask<string> Solve_2() => new(Part2());
 }
